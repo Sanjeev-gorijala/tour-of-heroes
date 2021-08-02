56a823d9cc6e0cc70b9e6720e40d060f25f4b3c4
sanjeev-gorijala <sanjeevg684@gmail.com>
2021-08-02 18:56:30 +0530

    Tour of heroes changes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Hero} from "../../Interfaces/Hero";
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Iron Man';
+ heroes: Hero[] = [
+    { id: 1, name: 'Dr Strange' },
+    { id: 2, name: 'Hulk' },
+    { id: 3, name: 'Wanda' },
+    { id: 4, name: 'Vision' },
+    { id: 5, name: 'IronMan' },
+    { id: 6, name: 'Captain America' },
+    { id: 7, name: 'Thor' },
+    { id: 8, name: 'Loki' },
+    { id: 9, name: 'Hawk Eye' },
+    { id: 10, name: 'Thanos' }
+  ];
 
+  selectedHero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero=hero;
   }
 
 }
